67df0f8b9d7ec0b7c127158e735f72321d356a7d
Cherny D. C. Berbesí I <cherny.berbesi@gmail.com>
2015-07-10 11:29:28 -0600

    CR[U]D

--- static/js/controller.js
+++ static/js/controller.js
@@ -4,11 +4,34 @@ djangoVEControllers.controller("taskController", ["$scope", "Task", function ($s
     "use strict";
     $scope.tasks = Task.read();
 
-    $scope.create = function () {
-        Task.create($scope.task, function (value, responseHeaders) {
-            $scope.tasks = Task.read();
-            $scope.task = {};
-            Materialize.toast("New Task has been added successfully", 4000)
-        });
-    }
+    var success = function (value, responseHeaders) {
+        $scope.tasks = Task.read();
+        $scope.task = {};
+        Materialize.toast("Task has been saved successfully", 4000)
+    };
+
+    var error = function (value, responseHeaders) {
+        $scope.tasks = Task.read();
+        $scope.task = {};
+        Materialize.toast("Oops, something wrong happened", 4000)
+    };
+
+    $scope.save = function () {
+        if ($scope.task.id === undefined) { // New task
+            Task.create($scope.task, success);
+        }
+        else { // Update task
+            Task.update({pk: $scope.task.id}, $scope.task, success)
+        }
+    };
+
+
+    $scope.get = function (id) {
+        $scope.isNew = false;
+        $("#modal").openModal();
+        $scope.task = $scope.tasks.filter(function (value) {
+            return id === value.id;
+        })[0];
+    };
+
 }]);
--- static/js/services.js
+++ static/js/services.js
@@ -2,8 +2,9 @@ var djangoVEServices = angular.module("djangoVEServices", ["ngResource"]);
 
 djangoVEServices.factory("Task", ["$resource",
     function ($resource) {
-        return $resource("api/tasks/", {}, {
+        return $resource("api/tasks/:pk", {}, {
             create: {method: "POST"},
-            read: {method: "GET", isArray: true}
+            read: {method: "GET", isArray: true},
+            update: {method: "PUT"}
         });
     }]);
